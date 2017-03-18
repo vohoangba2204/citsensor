4998a722f9d8c8ea326ef29d53de82a62f3a5f17
vohoangba2204 <vohoangba1995@gmail.com>
2017-03-18 23:09:18 +0700

    a

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-var a =1;
+var express = require('express');//khoi tao moduel express
+var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
+
+var db = mongojs('mydb', ['sensor']);//db:test,colection:
+var app = express();
+var path = require('path');
+
+app.use(bodyParser.json());//
+app.use('/public', express.static('public'));
